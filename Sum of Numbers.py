209b4381b0eee469e0d9f6c0ce2cc65bfeb170c0
Shreehayakreevan <spideyhayak@gmail.com>
2024-05-24 12:31:44 +0530

    Add files via upload

---
+++ Sum of Numbers.py
@@ -0,0 +1,9 @@
+print("Sum of two numbers")
+num1=float(input("Enter the First Number = "))
+num2=float(input("Enter the Second Number = "))
+sum=num1+num2
+print("Sum of Numbers",num1," + ",num2,"=",sum)
+num3=num1+num2
+num4=float(input("Enter the Fourth Number = "))
+sum=num3+num4
+print("Sum of Numbers",num3," + ",num4,"=",sum)
